6e68070d1742c4a3e3d2e639e4b7e6650cc2be04
guillermo_salazar <guillermos_2007@yahoo.com>
2015-06-24 15:19:40 +0000

    added image uploading for recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,11 +1,13 @@
 class Recipe < ActiveRecord::Base
-    validates :name,         presence: true, length: {minimum: 2}
-    validates :category,     presence: true, length: {minimum: 2}
-    validates :instructions, presence: true
-    validates :prep_time,    presence: true, numericality:{greater_than: 0}
-    validates :cook_time,    presence: true, numericality:{greater_than: 0}
-    
-    def minutes_total
-       cook_time + prep_time 
-    end
+  validates :name,         presence: true, length: {minimum: 2}
+  validates :category,     presence: true, length: {minimum: 2}
+  validates :instructions, presence: true
+  validates :prep_time,    presence: true, numericality:{greater_than: 0}
+  validates :cook_time,    presence: true, numericality:{greater_than: 0}
+  
+  mount_uploader :picture, PictureUploader
+  
+  def minutes_total
+     cook_time + prep_time 
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150624135528) do
+ActiveRecord::Schema.define(version: 20150624150014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150624135528) do
     t.integer  "cook_time"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "picture"
   end
 
 end
