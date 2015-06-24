40058f3d742caeb8313f608eb93ea8293a203915
guillermo_salazar <guillermos_2007@yahoo.com>
2015-06-24 14:52:58 +0000

    Added Validation and updated index for recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,11 @@
 class Recipe < ActiveRecord::Base
+    validates :name,         presence: true, length: {minimum: 2}
+    validates :category,     presence: true, length: {minimum: 2}
+    validates :instructions, presence: true
+    validates :prep_time,    presence: true, numericality:{greater_than: 0}
+    validates :cook_time,    presence: true, numericality:{greater_than: 0}
+    
+    def minutes_total
+       cook_time + prep_time 
+    end
 end
--- db/migrate/20150624135528_create_recipes.rb
+++ db/migrate/20150624135528_create_recipes.rb
@@ -1,10 +1,10 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
-      t.string :name
-      t.string :category
-      t.string :ingredients
-      t.text :instructions
+      t.string  :name
+      t.string  :category
+      t.string  :ingredients
+      t.text    :instructions
       t.integer :prep_time
       t.integer :cook_time
 
